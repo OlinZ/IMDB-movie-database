dd9dea948f40fc92cd4ace0a5a60a10632c7845c
Trevor Moore <moore.trev@tamu.edu>
2019-10-08 09:43:17 -0500

    structure the code

--- mainWindow.java
+++ mainWindow.java
@@ -1,69 +1,266 @@
-import javax.swing.*;
-import java.awt.event.*;
+// package com.mycompany.myproject1;
 import java.util.*;
+import java.lang.Object;
 
-public class mainWindow extends JFrame{
+public class MainWindow extends javax.swing.JFrame {
+    //Variables              
+    private javax.swing.JRadioButton chn_rbtn;
+    private javax.swing.JButton clear_btn;
+    private javax.swing.JRadioButton eng_rbtn;
+    private javax.swing.JRadioButton frc_rbtn;
+    private javax.swing.JRadioButton grmn_rbtn;
+    private javax.swing.JRadioButton itl_rbtn;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JRadioButton other_rbtn;
+    private javax.swing.JButton search_btn;
+    private javax.swing.JTextField search_ins;
+    private javax.swing.JButton simpleSearch_btn;
+    private javax.swing.JTextField yearFrom_ins;
+    private javax.swing.JTextField yearTo_ins;
 
-    private final int WIDTH = 500;
-    private final int HEIGHT = 300;
-    private JTextField input;
-    private JLabel text;
-    private JPanel panel;
-
-    public mainWindow () {
-        setTitle("Movie Searching");
-        setSize(WIDTH, HEIGHT);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panel = new JPanel();
-        input = new JTextField(20);
-        JButton query = new JButton("Query");
-        query.addActionListener(new CalcButtonListener());
-        // panel.add(text);
-        panel.add(input);
-        panel.add(query);
-        add(panel);
-
-
-        setVisible(true);
-
+  private DatabaseConnection databaseConnection;
+    public NewJFrame() {
+        databaseConnection = new DatabaseConnection();
+        initComponents();
     }
 
-    private class CalcButtonListener implements ActionListener
-   {
-      /**
-         The actionPerformed method executes when the user
-         clicks on the Calculate button.
-         @param e The event object.
-      */
 
-      public void actionPerformed(ActionEvent e)
-      {
-         String inputQuery;  // To hold the user's input
-         List<String> results;  // The number of miles
+   @SuppressWarnings("unchecked")
+   private void initComponents() {
 
-         // Get the text entered by the user into the
-         // text field.
-         inputQuery = input.getText();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        search_ins = new javax.swing.JTextField();
+        search_btn = new javax.swing.JButton();
+        clear_btn = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        eng_rbtn = new javax.swing.JRadioButton();
+        chn_rbtn = new javax.swing.JRadioButton();
+        itl_rbtn = new javax.swing.JRadioButton();
+        grmn_rbtn = new javax.swing.JRadioButton();
+        frc_rbtn = new javax.swing.JRadioButton();
+        other_rbtn = new javax.swing.JRadioButton();
+        simpleSearch_btn = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        yearFrom_ins = new javax.swing.JTextField();
+        yearTo_ins = new javax.swing.JTextField();
 
-         // Convert the input to miles.
-         results = jdbcpostgreSQL.getResults(inputQuery);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-         // Display the result.
-         text = new JLabel(results.toString());
-         JOptionPane.showMessageDialog(null, results.toString());
-         System.out.println(results.toString());
-         panel.add(new JLabel(results.toString()));
-         validate();
-         repaint();
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Movie Search"));
 
-         // For debugging, display a message indicating
-         // the application is ready for more input.
-         System.out.println("Ready for the next input.");
-      }
-   } // End of CalcButtonListener class
+        jLabel1.setFont(new java.awt.Font("Lucida Grande", 0, 18)); 
+        jLabel1.setText("Search");
 
-    public static void main(String[] args) {
-        new mainWindow();
-        // System.out.println(jdbcpostgreSQL.getResults("Select * from actors limit 3"));
-    }
+        search_btn.setText("Advanced Search");
+        search_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                search_btnActionPerformed(evt);
+            }
+        });
+
+        clear_btn.setText("Clear");
+        clear_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clear_btnActionPerformed(evt);
+            }
+        });
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder("Language"));
+
+        eng_rbtn.setText("English");
+
+        chn_rbtn.setText("Chinees");
+
+        itl_rbtn.setText("Italian");
+
+        grmn_rbtn.setText("German");
+
+        frc_rbtn.setText("French");
+
+        other_rbtn.setText("Other");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(eng_rbtn)
+                    .addComponent(chn_rbtn))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(itl_rbtn)
+                    .addComponent(grmn_rbtn))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(other_rbtn)
+                    .addComponent(frc_rbtn))
+                .addGap(21, 21, 21))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(eng_rbtn)
+                    .addComponent(itl_rbtn)
+                    .addComponent(frc_rbtn))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(chn_rbtn)
+                    .addComponent(grmn_rbtn)
+                    .addComponent(other_rbtn))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        simpleSearch_btn.setText("Simple Search");
+        simpleSearch_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                simpleSearch_btnActionPerformed(evt);
+            }
+        });
+
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder("Year"));
+
+        jLabel2.setText("From");
+
+        jLabel3.setText("To");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(yearFrom_ins)
+                    .addComponent(yearTo_ins, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(yearFrom_ins, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(yearTo_ins, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(clear_btn)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(search_btn))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(search_ins))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(simpleSearch_btn)))))
+                .addGap(411, 411, 411))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(search_ins, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(simpleSearch_btn)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(search_btn)
+                    .addComponent(clear_btn)))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 566, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }                       
+
+    private void clear_btnActionPerformed(java.awt.event.ActionEvent evt) {                                          
+       
+        search_ins.setText("");
+        yearFrom_ins.setText("");
+        yearTo_ins.setText("");
+        other_rbtn.setSelected(false);
+        eng_rbtn.setSelected(false);
+        frc_rbtn.setSelected(false);
+        grmn_rbtn.setSelected(false);
+        chn_rbtn.setSelected(false);
+        itl_rbtn.setSelected(false);
+
+
+    }                                         
+
+    private void simpleSearch_btnActionPerformed(java.awt.event.ActionEvent evt) { 
+        String name = search_ins.getText();
+        String query = "SELECT * FROM title_basics WHERE upper(primarytitle) ~ ";
+        query += "\'.*" + name.toUpperCase() + ".*\'";
+        if (!yearFrom_ins.getText().equals("") || !yearTo_ins.getText().equals("")) {
+            query += " AND startyear between " + yearFrom_ins.getText();
+            query += " AND " + yearTo_ins.getText();
+        }
+        System.out.println(query);
+        List<String> results = databaseConnection.getResults(query);
+        System.out.println(results);
+    }                                                
+
+    private void search_btnActionPerformed(java.awt.event.ActionEvent evt) {                                           
+   }                                          
+
+
+
+
 }
+
+
